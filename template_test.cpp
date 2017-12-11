de97a2a54bd946a3c9c5be4dca4c91fe6472cfa7
linx <linx@meixing.com>
2017-12-11 20:52:47 +0800

    add template_test.cpp

---
+++ template_test.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <map>
+#include <set>
+
+using namespace std;
+
+template <typename K, typename V>
+class map_template
+{
+public:
+    map_template() {}
+    ~map_template() {}
+
+    virtual void Insert(const K &key, const V &value)
+    {
+        this->data_[key] = value;
+    }
+
+protected:
+    map<K, V> data_;
+};
+
+
+template <typename K, typename V>
+class map_set_template : public map_template<K, set<V> >
+{
+public:
+    map_set_template() {}
+    ~map_set_template() {}
+
+    void Insert(const K &key, const V &value)
+    {
+        this->data_[key].insert(value);
+    }
+};
+
+
+int main()
+{
+    map_set_template<int, int> data;
+    data.Insert(1,2);
+    return 0;
+}

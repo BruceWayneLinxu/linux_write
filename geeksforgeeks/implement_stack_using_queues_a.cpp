cc16457ab6a2dc2998b94a916e0d9955687be2f9
BruceWayneLinxu <BruceWayneLinxu@users.noreply.github.com>
2018-02-12 11:35:36 +0800

    Add files via upload

---
+++ geeksforgeeks/implement_stack_using_queues_a.cpp
@@ -0,0 +1,103 @@
+// https://www.geeksforgeeks.org/implement-stack-using-queue/
+/* Program to implement a stack using 
+two queue */
+
+/*
+
+A stack can be implemented using two queues. Let stack to be implemented be ‘s’ and queues used to implement be ‘q1’ and ‘q2’. Stack ‘s’ can be implemented in two ways:
+
+Method 1 (By making push operation costly)
+This method makes sure that newly entered element is always at the front of ‘q1’, so that pop operation just dequeues from ‘q1’. ‘q2’ is used to put every new element at front of ‘q1’.
+
+push(s, x) // x is the element to be pushed and s is stack
+  1) Enqueue x to q2
+  2) One by one dequeue everything from q1 and enqueue to q2.
+  3) Swap the names of q1 and q2 
+// Swapping of names is done to avoid one more movement of all elements 
+// from q2 to q1. 
+
+pop(s)
+  1) Dequeue an item from q1 and return it.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+ 
+class Stack
+{ 
+    // Two inbuilt queues
+    queue<int> q1, q2;
+     
+    // To maintain current number of
+    // elements
+    int curr_size;
+ 
+    public:
+    Stack()
+    {
+        curr_size = 0;
+    }
+ 
+    void push(int x)
+    {
+        curr_size++;
+ 
+        // Push x first in empty q2
+        q2.push(x);
+ 
+        // Push all the remaining 
+        // elements in q1 to q2. 
+        while (!q1.empty())
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+ 
+        // swap the names of two queues
+        queue<int> q = q1;
+        q1 = q2;
+        q2 = q;
+    }
+ 
+    void pop(){
+ 
+        // if no elements are there in q1 
+        if (q1.empty())
+            return ;
+        q1.pop();
+        curr_size--;
+    }
+ 
+    int top()
+    {
+        if (q1.empty())
+            return -1;
+        return q1.front();
+    }
+ 
+    int size()
+    {
+        return curr_size;
+    }
+};
+ 
+// driver code
+int main()
+{
+    Stack s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+ 
+    cout << "current size: " << s.size() 
+         << endl;
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+ 
+    cout << "current size: " << s.size() 
+         << endl;
+    return 0;
+}
+// This code is contributed by Chhavi 
